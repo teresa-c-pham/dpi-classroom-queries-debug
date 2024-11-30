4daa59fafd359be56180b49d044386b423ec2ff7
Teresa Pham <79329784+teresa-c-pham@users.noreply.github.com>
2024-11-30 20:47:22 +0000

    debug courses, enrollment, department, and students queries

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,12 @@ Rails.application.routes.draw do
   #------------------------------
   # Department routes
 
-  get("/departments", { :controller => "department", :action => "index" })
+  get("/departments", { :controller => "departments", :action => "index" })
   
   get("/departments/:path_id", { :controller => "departments", :action => "show" })
 
   post("/insert_department", { :controller => "departments", :action => "create" })
 
-  get("/departments", { :controller => "departments", :action => "index" })
-  
   post("/modify_department/:path_id", { :controller => "departments", :action => "update" })
 
   get("/delete_department/:path_id", { :controller => "departments", :action => "destroy" })
@@ -23,11 +21,9 @@ Rails.application.routes.draw do
   
   get("/courses/:path_id", { :controller => "courses", :action => "show" })
 
-  get("/insert_course", { :controller => "courses", :action => "create" })
+  post("/insert_course", { :controller => "courses", :action => "create" })
 
-  get("/courses", { :controller => "courses_controller", :action => "index" })
-  
-  get("/modify_course/:path_id", { :controller => "courses", :action => "update" })
+  post("/modify_course/:path_id", { :controller => "courses", :action => "update" })
 
   get("/delete_course/:path_id", { :controller => "courses", :action => "destroy" })
 
@@ -40,8 +36,6 @@ Rails.application.routes.draw do
 
   post("/insert_student", { :controller => "students", :action => "create" })
 
-  get("/students", { :controller => "students", :action => "index" })
-  
   post("/modify_student/:path_id", { :controller => "students", :action => "update" })
 
   get("/delete_student/:path_id", { :controller => "students", :action => "destroy" })
